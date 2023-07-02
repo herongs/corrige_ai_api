3b80378cd1b8ee88611c15509f7a95c2440b8ba2
Heron Ghellere Slovinski <heronslovinski@hotmail.com>
2023-07-02 19:36:43 -0300

    fix auth usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use OwenIt\Auditing\Contracts\Auditable;
-class Usuario extends Model implements Auditable
+class Usuario extends Authenticatable implements  Auditable
 {
+  use SoftDeletes, \OwenIt\Auditing\Auditable;
     use \OwenIt\Auditing\Auditable;
     protected $table = 'usuarios';
     protected $hidden = ['password'];
